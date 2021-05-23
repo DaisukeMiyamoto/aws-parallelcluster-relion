63259dffe5112ea47b3cd3f695197e85f1a4c8f9
Daisuke Miyamoto <midaisuk@gmail.com>
2021-05-24 07:23:51 +0900

    update template

--- gui_batch_template_slurm.sh
+++ gui_batch_template_slurm.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-#SBATCH --nodes=XXXmpinodesXXX
-#--SBATCH --ntasks-per-node=XXXextra1XXX
+#SBATCH --ntasks=XXXmpinodesXXX
 #SBATCH --cpus-per-task=XXXthreadsXXX
 #SBATCH --partition=XXXqueueXXX
-#SBATCH --error=XXXerrfileXXX
 #SBATCH --output=XXXoutfileXXX
+#SBATCH --error=XXXerrfileXXX
 #SBATCH --open-mode=append
-##SBATCH XXXextra4XXX
-##SBATCH XXXextra5XXX
-##SBATCH XXXextra6XXX
-##SBATCH --time=XXXextra1XXX
-##SBATCH --mem-per-cpu=XXXextra2XXX
-##SBATCH --gres=XXXextra3XXX
+#SBATCH --time=XXXextra1XXX
+#SBATCH --mem-per-cpu=XXXextra2XXX
+#SBATCH --gres=XXXextra3XXX
+#SBATCH XXXextra4XXX
+#SBATCH XXXextra5XXX
+#SBATCH XXXextra6XXX
 
-#export OMPI_MCA_btl_tcp_if_exclude="docker0,lo,virbr0"
-time srun --mpi=pmix XXXcommandXXX
+srun --mpi=pmix XXXcommandXXX
