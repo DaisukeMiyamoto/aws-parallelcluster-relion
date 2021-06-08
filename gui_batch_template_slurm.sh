d73a2f0b9811aa98e6c08e75c045ece431ed9684
Daisuke Miyamoto <midaisuk@gmail.com>
2021-06-08 16:11:50 +0900

    update job template

--- gui_batch_template_slurm.sh
+++ gui_batch_template_slurm.sh
@@ -2,14 +2,15 @@
 #SBATCH --ntasks=XXXmpinodesXXX
 #SBATCH --cpus-per-task=XXXthreadsXXX
 #SBATCH --partition=XXXqueueXXX
-#SBATCH --output=XXXoutfileXXX
 #SBATCH --error=XXXerrfileXXX
+#SBATCH --output=XXXoutfileXXX
 #SBATCH --open-mode=append
-#SBATCH --time=XXXextra1XXX
-#SBATCH --mem-per-cpu=XXXextra2XXX
-#SBATCH --gres=XXXextra3XXX
-#SBATCH XXXextra4XXX
-#SBATCH XXXextra5XXX
-#SBATCH XXXextra6XXX
+##SBATCH XXXextra4XXX
+##SBATCH XXXextra5XXX
+##SBATCH XXXextra6XXX
+##SBATCH --time=XXXextra1XXX
+##SBATCH --mem-per-cpu=XXXextra2XXX
+##SBATCH --gres=XXXextra3XXX
 
-srun --mpi=pmix XXXcommandXXX
+export PATH=$PATH:/lustre/relion/build-gpu/bin/
+time srun --mpi=pmix XXXcommandXXX
